ea4490df8b2fb7afecb93816d01b30cc45254d9a
Sergio Ernesto López Orellana <a2017115053@estudiantes.upsa.edu.bo>
2021-03-20 10:24:15 -0400

    Creacion del modelo Producto

---
+++ Examen_Parcial/Parcial I Sergio Lopez/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial_I_Sergio_Lopez.Models
+{
+    public class Producto
+    {
+        [Key]
+        [StringLength(99999,MinimumLength =1,ErrorMessage ="Rango 1 a 99999")]
+        public int ProductoID { get; set; }
+        [Required]
+        [StringLength(30,MinimumLength =3,ErrorMessage ="Ingrese entre 3 a 30 caracteres")]
+        public string Description { get; set; }
+        [Required]
+        public DataType Currency { get; set; }
+        public DataType Fecha { get; set; }
+    }
+}
