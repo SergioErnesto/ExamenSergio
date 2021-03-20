f4d389027562426af69b570f704fab2e57b3c705
Sergio Ernesto López Orellana <a2017115053@estudiantes.upsa.edu.bo>
2021-03-20 10:55:15 -0400

    Creacion del Controlador

---
+++ Examen_Parcial/Parcial I Sergio Lopez/Controllers/ParcialCalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Parcial_I_Sergio_Lopez.Controllers
+{
+    public class ParcialCalculatorController : ApiController
+    {
+        [HttpPost]
+        [Route("Add")]
+        public int Add([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa + numb;
+        }
+    }
+}
